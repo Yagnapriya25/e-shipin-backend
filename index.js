66c62e6c04a2b128dfe82c8c14672f2a2d70758a
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-16 22:05:07 +0530

    first commit

--- index.js
+++ index.js
@@ -24,27 +24,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 
-const allowedOrigins = [
-    'http://localhost:3000', // Replace with your local development URL
-    'https://your-production-domain.com' // Replace with your production URL
-  ];
-  
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS')); // Reject the request
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Allow credentials (like cookies)
-    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-  };
-  
-  // Use CORS with the specified options
-  app.use(cors(corsOptions));
+app.use(cors({}));
 
 app.use(bodyParser.json());
 
