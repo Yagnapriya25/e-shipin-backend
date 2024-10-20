3ae0795f8623864adb2f0d7d7c76cc5de2f4bb6c
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-20 18:59:32 +0530

    first commit

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -308,12 +308,18 @@ dotenv.config();
 
 const router = express.Router();
 
+
+
 // Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+const storage = multer.memoryStorage();
+const uploads = multer({ storage: storage });
 
 // Nodemailer configuration
 const transporter = nodemailer.createTransport({
