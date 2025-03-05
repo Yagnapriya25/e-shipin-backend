c188aba662be00ba72f4593a08ded1e4a5372298
karthikn25 <mkarthikeyan179@gmail.com>
2025-03-05 16:15:26 +0530

    ping add

--- index.js
+++ index.js
@@ -1,54 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const { dbConnection } = require('./db');
-// const path = require("path");
-// const { userRouter } = require('./Router/userRouter.js');
-// const { categoryRouter } = require('./Router/categoryRouter.js');
-// const { productRouter } = require('./Router/productRouter.js');
-// const { cartRouter } = require('./Router/cartRouter.js');
-// const { addressRouter } = require('./Router/addressRouter.js');
-// const { orderRouter } = require('./Router/OrderRouter.js');
-
-
-// // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-// dotenv.config();
-
-
-// const app = express();
-
-
-// const PORT = process.env.PORT || 8080;
-
-// app.use(express.json())
-
-// app.use(cors({
-//     origin: '*', // Allow only your frontend origin
-//     methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Specify allowed methods
-//     credentials: true // Allow credentials if needed
-// }));
-
-// app.use(bodyParser.json());
-
-// dbConnection();
-
-// app.use("/uploads",express.static(path.join(__dirname,"uploads")))
-
-// app.use("/api/user",userRouter);
-
-// app.use("/api/category",categoryRouter)
-
-// app.use("/api/product",productRouter);
-
-// app.use("/api/cart",cartRouter);
-
-// app.use("/api/address",addressRouter)
-
-// app.use("/api/order",orderRouter);
-
-// app.listen(PORT,()=>console.log(`localhost running under:${PORT}`))
 
 const express = require('express');
 const dotenv = require('dotenv');
@@ -90,6 +39,10 @@ dbConnection();
 
 // Serve static files from the "uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get('/ping', (req, res) => {
+    res.status(200).send('Service is awake');
+  });
 
 // Routes
 app.use("/api/user", userRouter);
